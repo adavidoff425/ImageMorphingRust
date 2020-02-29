866de730792324b8bfb3e412de490faee27e46b2
Alex Davidoff <ad35@pdx.edu>
2020-02-29 12:08:24 -0800

    created second window for dst image after closing src window

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ const FRAGMENT_SHADER: &'static str = r#"
 fn main() {
 //  assert_eq!(env::args().count(), 3);
   let src = std::env::args().nth(1).unwrap();
+  let dst = std::env::args().nth(2).unwrap();
 
   // Boilerplate code for initilizing glium display window
   // Adapted from tutorial at docs.rs/glium/0.26.0/glium
@@ -65,29 +66,31 @@ fn main() {
     .with_inner_size(dpi::LogicalSize::new(1024.0, 768.0))
     .with_title("Image Morphing Tool");
   let cb = ContextBuilder::new();
-  let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+  let display_src = glium::Display::new(wb, cb, &events_loop).unwrap();
 
-  let imgs = {
-//    let dst = env::args().nth(2).unwrap();
+  let src = {
     let src_img = image::open(&Path::new(&src)).unwrap().to_rgba();
-//  let dst_img = image::open(&Path::new(&dst)).unwrap().to_rgba();
     let src_dim = src_img.dimensions();
-//    let dst_dim = dst_img.dimensions();
     let src_img = texture::RawImage2d::from_raw_rgba_reversed(&src_img.into_raw(), src_dim);
- //   let dst_img = texture::RawImage2d::from_raw_rgba_reversed(&dst_img.into_raw(), dst_dim);
-    texture::SrgbTexture2d::new(&display, src_img).unwrap()
-  //  let dst_map = texture::SrgbTexture2d::new(&display, dst_img).unwrap();
+    texture::SrgbTexture2d::new(&display_src, src_img).unwrap()
+  };
+
+  let dst = {
+    let dst_img = image::open(&Path::new(&dst)).unwrap().to_rgba();
+    let dst_dim = dst_img.dimensions();
+    let dst_img = texture::RawImage2d::from_raw_rgba_reversed(&dst_img.into_raw(), dst_dim);
+    texture::SrgbTexture2d::new(&display_dst, dst_img).unwrap()
   };
 
   let (vertices, indices) = {
     let data: Vec<u16> = vec![0, 1, 2, 1, 3, 2];
-    let vertexBuf = glium::VertexBuffer::empty_dynamic(&display, 4).unwrap();
-    let indexBuf = glium::IndexBuffer::new(&display, index::PrimitiveType::TrianglesList, &data).unwrap();
-    (vertexBuf, indexBuf)
+    let vertex_buf = glium::VertexBuffer::empty_dynamic(&display_src, 4).unwrap();
+    let index_buf = glium::IndexBuffer::new(&display_src, index::PrimitiveType::TrianglesList, &data).unwrap();
+    (vertex_buf, index_buf)
   };
 
   let program = glium::Program::from_source(
-    &display,
+    &display_src,
     VERTEX_SHADER, 
     FRAGMENT_SHADER, 
     None
@@ -105,7 +108,7 @@ fn main() {
     Into::<[[f32; 4]; 4]>::into(matrix)
   };
 
-  let size = Vector2 { x: 500.0, y: 500.0 };
+  let size = Vector2 { x: 500.0, y: 350.0 };
   let mut position = Vector2 { x: 512.0, y: 384.0 };
   
   events_loop.run(move |event, _, control_flow| {
@@ -128,7 +131,7 @@ fn main() {
       _ => return,
     }
 
-    let mut target = display.draw();
+    let mut target = display_src.draw();
     target.clear_color(1.0, 1.0, 1.0, 1.0);
 
     {
@@ -136,19 +139,90 @@ fn main() {
       let right = position.x + size.x / 2.0;
       let bottom = position.y + size.y / 2.0;
       let top = position.y - size.y / 2.0;
-      let vertexBuffer = vec![
+      let vertex_buf = vec![
         Vertex { position: [left, top] },
         Vertex { position: [right, top] },
         Vertex { position: [left, bottom] },
         Vertex { position: [right, bottom] }
       ];
-      vertices.write(&vertexBuffer);
+      vertices.write(&vertex_buf);
     }
 
     {
       let uniforms = uniform! {
         projection: perspective,
-        tex: &imgs,
+        tex: &src,
+      };
+
+      target.draw(
+        &vertices,
+        &indices, 
+        &program,
+        &uniforms,
+        &Default::default()
+      ).unwrap();
+    }
+    target.finish().unwrap();
+  });
+
+  let events_loop = event_loop::EventLoop::new();
+  let display_dst = glium::Display::new(wb, cb, &events_loop).unwrap();
+  
+  let (vertices, indices) = {
+    let data: Vec<u16> = vec![0, 1, 2, 1, 3, 2];
+    let vertex_buf = glium::VertexBuffer::empty_dynamic(&display_dst, 4).unwrap();
+    let index_buf = glium::IndexBuffer::new(&display_dst, index::PrimitiveType::TrianglesList, &data).unwrap();
+    (vertex_buf, index_buf)
+  };
+
+  let program = glium::Program::from_source(
+    &display_dst,
+    VERTEX_SHADER, 
+    FRAGMENT_SHADER, 
+    None
+  ).unwrap();
+  
+  events_loop.run(move |event, _, control_flow| {
+    let next_frame_time = Instant::now() + Duration::from_nanos(16_666_667);
+    *control_flow = event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+    match event {
+      event::Event::WindowEvent { event, .. } => match event {
+          event::WindowEvent::CloseRequested => {
+            *control_flow = event_loop::ControlFlow::Exit;
+            return;
+          },
+          _ => return,
+      },
+      event::Event::NewEvents(cause) => match cause {
+        event::StartCause::ResumeTimeReached { .. } => (),
+        event::StartCause::Init => (),
+        _ => return,
+      },
+      _ => return,
+    }
+
+    let mut target = display_dst.draw();
+    target.clear_color(1.0, 1.0, 1.0, 1.0);
+
+    {
+      let left = position.x - size.x / 2.0;
+      let right = position.x + size.x / 2.0;
+      let bottom = position.y + size.y / 2.0;
+      let top = position.y - size.y / 2.0;
+      let vertex_buf = vec![
+        Vertex { position: [left, top] },
+        Vertex { position: [right, top] },
+        Vertex { position: [left, bottom] },
+        Vertex { position: [right, bottom] }
+      ];
+      vertices.write(&vertex_buf);
+    }
+
+    {
+      let uniforms = uniform! {
+        projection: perspective,
+        tex: &dst,
       };
 
       target.draw(
