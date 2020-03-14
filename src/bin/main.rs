89ba9fa9d14ecf4938a19ed76faba343872b3139
Alex Davidoff <ad35@pdx.edu>
2020-03-14 15:16:06 -0700

    fixed bug clearing vectors in warp loop

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -63,10 +63,10 @@ fn ft_line_err() -> ! {
 fn main() {
     let src_line = vec![
         Vertex {
-            position: [641f64, 210f64],
+            position: [541f64, 160f64],
         },
         Vertex {
-            position: [766f64, 214f64],
+            position: [616f64, 164f64],
         },
     ];
     let src_line2 = vec![
@@ -79,19 +79,19 @@ fn main() {
     ];
     let src_line3 = vec![
         Vertex {
-            position: [394f64, 470f64],
+            position: [294f64, 470f64],
         },
         Vertex {
-            position: [572f64, 471f64],
+            position: [472f64, 471f64],
         },
     ];
 
     let dst_line = vec![
         Vertex {
-            position: [639f64, 173f64],
+            position: [539f64, 123f64],
         },
         Vertex {
-            position: [811f64, 250f64],
+            position: [711f64, 200f64],
         },
     ];
     let dst_line2 = vec![
@@ -104,14 +104,14 @@ fn main() {
     ];
     let dst_line3 = vec![
         Vertex {
-            position: [394f64, 438f64],
+            position: [294f64, 438f64],
         },
         Vertex {
-            position: [791f64, 466f64],
+            position: [591f64, 466f64],
         },
     ];
-    let src_lines = vec![src_line, src_line2];
-    let dst_lines = vec![dst_line, dst_line2];
+    let src_lines = vec![src_line, src_line2, src_line3];
+    let dst_lines = vec![dst_line, dst_line2, dst_line3];
     let src_path = std::env::args().nth(1).unwrap();
     let dst_path = std::env::args().nth(2).unwrap();
     let src = image::open(&Path::new(&src_path)).unwrap();
@@ -119,19 +119,19 @@ fn main() {
     let (w, h) = dst.dimensions();
     let src = src.resize_exact(w, h, image::imageops::FilterType::Nearest).to_rgba();
     let morph = Morph::new(
-        &src, &dst, &src_lines, &dst_lines, (0.5f64, 1.0f64, 50.0f64, 1.0f64),
-    );/*
+        &src, &dst, &src_lines, &dst_lines, (0.5f64, 0.5f64, 50.0f64, 1.5f64),
+    );
     let inter_line_morph = morph.interpolate_lines();
-    let x: f64 = 50.0;
-    let y: f64 = 50.0;
+    let x: f64 = 150.0;
+    let y: f64 = 250.0;
     let new_pt = morph.warp(x, y, &inter_line_morph, morph.src_lines.to_vec());
     println!("({}, {})", new_pt.0, new_pt.1);
     let new_pt = morph.warp(x, y, &inter_line_morph, morph.dst_lines.to_vec());
-    println!("({}, {})", new_pt.0, new_pt.1);*/
-    let morphed: RgbaImage = morph.morph();
+    println!("({}, {})", new_pt.0, new_pt.1);
+  /*  let morphed: RgbaImage = morph.morph();
     let image = image::DynamicImage::ImageRgba8(morphed).flipv();
     image.save("morphed.png").unwrap();
-   /* if std::env::args().count() != 3 {
+    if std::env::args().count() != 3 {
         arg_error();
     }
     let src_path = std::env::args().nth(1).unwrap();
